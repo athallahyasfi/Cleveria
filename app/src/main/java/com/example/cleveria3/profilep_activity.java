5a0cec0a503f8f9c97f077877b5e9bf4f217108f
peregrinate <125367455+peregrinate@users.noreply.github.com>
2024-10-10 00:30:47 +0700

    Add files via upload
    Layout for profile page.

---
+++ app/src/main/java/com/example/cleveria3/profilep_activity.java
@@ -0,0 +1,114 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		profilep
+	 *	@date 		Wednesday 09th of October 2024 04:45:32 PM
+	 *	@title 		Page 3
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class profilep_activity extends Activity {
+
+	
+	private View _bg__profilep_ek2;
+	private View pencil;
+	private ImageView multilprofile;
+	private View backprof;
+	private TextView username;
+	private TextView nimuser;
+	private TextView points;
+	private TextView quiz_taken;
+	private TextView pointtaken;
+	private TextView quizattempt;
+	private TextView rank;
+	private TextView userrank;
+	private View canvasc;
+	private TextView recent_quiz;
+	private TextView statsone;
+	private TextView informationone;
+	private View back;
+	private View selectp;
+	private TextView profilep_ek3;
+	private ImageView head;
+	private TextView completematt;
+	private TextView statstwo;
+	private TextView infortwo;
+	private TextView completion_matt;
+	private ImageView staricon;
+	private ImageView pencilp;
+	private ImageView medal;
+	private ImageView half;
+	private ImageView block;
+	private ImageView completiont;
+	private ImageView barrier;
+	private ImageView leaderboardp;
+	private ImageView homep;
+	private ImageView profilepict;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profilep);
+
+		
+		_bg__profilep_ek2 = (View) findViewById(R.id._bg__profilep_ek2);
+		pencil = (View) findViewById(R.id.pencil);
+		multilprofile = (ImageView) findViewById(R.id.multilprofile);
+		backprof = (View) findViewById(R.id.backprof);
+		username = (TextView) findViewById(R.id.username);
+		nimuser = (TextView) findViewById(R.id.nimuser);
+		points = (TextView) findViewById(R.id.points);
+		quiz_taken = (TextView) findViewById(R.id.quiz_taken);
+		pointtaken = (TextView) findViewById(R.id.pointtaken);
+		quizattempt = (TextView) findViewById(R.id.quizattempt);
+		rank = (TextView) findViewById(R.id.rank);
+		userrank = (TextView) findViewById(R.id.userrank);
+		canvasc = (View) findViewById(R.id.canvasc);
+		recent_quiz = (TextView) findViewById(R.id.recent_quiz);
+		statsone = (TextView) findViewById(R.id.statsone);
+		informationone = (TextView) findViewById(R.id.informationone);
+		back = (View) findViewById(R.id.back);
+		selectp = (View) findViewById(R.id.selectp);
+		profilep_ek3 = (TextView) findViewById(R.id.profilep_ek3);
+		head = (ImageView) findViewById(R.id.head);
+		completematt = (TextView) findViewById(R.id.completematt);
+		statstwo = (TextView) findViewById(R.id.statstwo);
+		infortwo = (TextView) findViewById(R.id.infortwo);
+		completion_matt = (TextView) findViewById(R.id.completion_matt);
+		staricon = (ImageView) findViewById(R.id.staricon);
+		pencilp = (ImageView) findViewById(R.id.pencilp);
+		medal = (ImageView) findViewById(R.id.medal);
+		half = (ImageView) findViewById(R.id.half);
+		block = (ImageView) findViewById(R.id.block);
+		completiont = (ImageView) findViewById(R.id.completiont);
+		barrier = (ImageView) findViewById(R.id.barrier);
+		leaderboardp = (ImageView) findViewById(R.id.leaderboardp);
+		homep = (ImageView) findViewById(R.id.homep);
+		profilepict = (ImageView) findViewById(R.id.profilepict);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
